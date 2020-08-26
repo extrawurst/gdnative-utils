c7ec5717a31659811a1c8ef4389f02c57d93916e
unknown <friedrichschirme@gmail.com>
2020-08-26 10:42:48 +0200

    add missing file

---
+++ src/extensions/safe.rs
@@ -0,0 +1,39 @@
+use gdnative::{
+    object::{AssumeSafeLifetime, LifetimeConstraint},
+    prelude::*,
+};
+
+pub trait RefExtension<T: GodotObject> {
+    fn safe<'a, 'r>(&'r self) -> TRef<'a, T, Shared>
+    where
+        AssumeSafeLifetime<'a, 'r>: LifetimeConstraint<T::RefKind>;
+}
+
+impl<T: GodotObject> RefExtension<T> for Ref<T> {
+    fn safe<'a, 'r>(&'r self) -> TRef<'a, T, Shared>
+    where
+        AssumeSafeLifetime<'a, 'r>: LifetimeConstraint<T::RefKind>,
+    {
+        unsafe { self.assume_safe() }
+    }
+}
+
+pub trait TRefExtension<'a, T: GodotObject> {
+    fn safe(self) -> TRef<'a, T, Shared>;
+}
+
+impl<'a, T: GodotObject> TRefExtension<'a, T> for TRef<'a, T, Shared> {
+    fn safe(self) -> TRef<'a, T, Shared> {
+        self
+    }
+}
+
+pub trait TRefNodeExtension<T: GodotObject> {
+    fn safe<'a>(&'a self) -> &'a T;
+}
+
+impl<T: GodotObject> TRefNodeExtension<T> for T {
+    fn safe<'a>(&'a self) -> &'a T {
+        self
+    }
+}
